e8e4d0a481760211e67057e352c942dc9c740d9f
Pawel Piotrowicz <pawel.piotrowicz@intel.com>
2022-04-06 15:16:15 +0200

    save

---
+++ OneDNN/build_one_api.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#git clone https://github.com/oneapi-src/oneDNN.git
+cd oneDNN
+
+if [ -f build ]; then
+ rm -rf build
+fi
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=`pwd`/../../onednn_install ..
+make -j20
+make install
+
+
+
